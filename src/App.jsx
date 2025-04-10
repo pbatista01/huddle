d6c198f884208eaf3cccce032ab0354ef8bc9aa1
Estefanie Gisell Peña Batista <estefanie.pena.b@gmail.com>
2025-04-10 13:29:42 -0500

    Desktop editions

--- src/App.jsx
+++ src/App.jsx
@@ -16,29 +16,29 @@ function App() {
     <>
       <Header />
       <Hero />
-      <div className="pt-[14px]">
       <Feature
         featureImg={growImg}
         featureTitle="Grow Together"
         featureDescription="Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form."
+        extraMargin="lg:w-[75%]"
       />
       <Feature
         featureImg={flowImg}
         featureTitle="Flowing Conversations"
         featureDescription="You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow."
-        extraOuterClass="pt-[66px]"
-        extraInnerClass="mt-[68px] pb-[52px]"
-        extraMargin="mt-[15px]"
+        extraOuterClass="pt-[66px] lg:mt-10"
+        extraInnerClass="mt-[68px] pb-[52px] lg:ml-[169px]"
+        extraMargin="mt-[15px] lg:w-[100%]"
+        changeOrderImg="lg:order-1"
       />
       <Feature
         featureImg={usersImg}
         featureTitle="Your Users"
         featureDescription="It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately."
-        extraOuterClass="mt-[39px] pt-[60px]"
+        extraOuterClass="mt-[39px] pt-[60px] lg:mt-10"
         extraInnerClass="mt-[63px]"
-        extraMargin="mt-[14px] w-[95%]"
+        extraMargin="mt-[14px] w-[95%] lg:w-[73%] "
       />
-      </div>
       <CTA />
       <Footer />
     </>
--- src/CTA.jsx
+++ src/CTA.jsx
@@ -1,8 +1,8 @@
 function CTA() {
     return (
-      <section className="w-[90%] h-auto mt-[94px] px-2 pt-[45px] pb-[40px] z-10 relative top-[67px] m-auto bg-white border rounded-xl shadow-[0_0_10px_rgba(0,0,0,0.1)] flex flex-col">
-        <h2 className="font-heading text-[16px] m-auto">Ready To Build Your Community?</h2>
-        <button className="w-[200px] h-10 bg-pink text-white font-heading text-[10px] rounded-full m-auto mt-6 font-medium shadow-xl hover:opacity-50">Get Started For Free</button>
+      <section className="w-[90%] lg:w-[56%] h-auto mt-[94px] lg:mt-[65px] px-2 pt-[45px] pb-[40px] lg:pt-[60px] lg:pb-[50px] z-10 relative top-[67px] lg:top-[95px] m-auto bg-white border rounded-xl shadow-[0_0_10px_rgba(0,0,0,0.1)] flex flex-col">
+        <h2 className="font-heading text-[16px] lg:text-[32px] m-auto">Ready To Build Your Community?</h2>
+        <button className="w-[200px] lg:w-[400px] lg:h-20 h-10 bg-pink text-white font-heading text-[10px] lg:text-[20px] rounded-full m-auto mt-6 lg:mt-9 font-medium shadow-xl hover:opacity-50">Get Started For Free</button>
       </section>
     );
   }
